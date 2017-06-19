604f451373b248df5be4fd12ec67835b47763d1f
Zireael07 <zi@mail.com>
2017-06-19 14:22:48 +0200

    basic player movement

--- main.py
+++ main.py
@@ -9,6 +9,29 @@ class struc_Tile:
     def __init__(self, block_path):
         self.block_path = block_path
 
+class obj_Actor:
+    def __init__(self, x, y, char):
+        self.x = x
+        self.y = y
+        self.char = char
+
+    def draw(self):
+        blt.put(self.x*constants.TILE_WIDTH, self.y*constants.TILE_HEIGHT, self.char)
+
+    def move(self, dx, dy):
+        if self.y + dy >= len(GAME_MAP):
+            print("Tried to move out of map")
+            return
+
+        if self.x + dx >= len(GAME_MAP[0]):
+            print("Tried to move out of map")
+            return
+
+        if GAME_MAP[self.x+dx][self.y+dy].block_path == False:
+            self.x += dx
+            self.y += dy
+
+
 def map_create():
     new_map = [[struc_Tile(False) for y in range(0, constants.MAP_HEIGHT)] for x in range(0, constants.MAP_WIDTH)]
 
@@ -22,6 +45,8 @@ def draw_game():
     # blt.printf(1, 1, 'Hello, world!')
 
     draw_map(GAME_MAP)
+
+    PLAYER.draw()
 
     # blt.refresh()
 
@@ -40,9 +65,23 @@ def game_main_loop():
     game_quit = False
 
     while not game_quit:
+
+        #clear
+        blt.clear()
+
         key = blt.read()
         if key in (blt.TK_ESCAPE, blt.TK_CLOSE):
             game_quit = True
+
+        if key == blt.TK_UP:
+            PLAYER.move(0,-1)
+        if key == blt.TK_DOWN:
+            PLAYER.move(0,1)
+        if key == blt.TK_LEFT:
+            PLAYER.move(-1,0)
+        if key == blt.TK_RIGHT:
+            PLAYER.move(1,0)
+
 
         # draw
         draw_game()
@@ -55,19 +94,25 @@ def game_main_loop():
 
 
 def game_initialize():
-    global GAME_MAP
+    global GAME_MAP, PLAYER
 
     blt.open()
-    blt.set("window: size=80x25, cellsize=auto, title='Veins of the Earth'; font: default")
+    # default terminal size is 80x25
+    blt.set("window: size=80x45, cellsize=auto, title='Veins of the Earth'; font: default")
+
+    blt.composition(True)
 
     # needed to avoid insta-close
     blt.refresh()
 
     #tiles
-    blt.set("0x02E: gfx/floor_sand.png")
-    blt.set("0x23: gfx/wall_stone.png")
+    blt.set("0x02E: gfx/floor_sand.png") # "."
+    blt.set("0x23: gfx/wall_stone.png") # "#"
+    blt.set("0x40: gfx/human_m.png") # "@"
 
     GAME_MAP = map_create()
+
+    PLAYER = obj_Actor(0,0, "@")
 
 if __name__ == '__main__':
     game_initialize()
