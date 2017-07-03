b6e4dee07e34ff1102639a458567040324f46a34
Zireael07 <zi@mail.com>
2017-07-03 11:20:09 +0200

    new: random item generator now rolls for armor material and weapon bonus

--- main.py
+++ main.py
@@ -448,6 +448,97 @@ def generate_item_rarity():
     #print chance_roll
     return chance_roll
 
+def generate_item_type():
+    chances = []
+    chances.append(("Item", 15)) # 17 in d20 SRD
+    chances.append(("Armor", 35)) # 35 in d20 SRD
+    chances.append(("Weapons", 35)) #22 in d20 SRD
+    chances.append(("Tools", 15)) #16 in d20 SRD
+
+    num = 0
+    chance_roll = []
+    for chance in chances:
+        old_num = num + 1
+        num += 1 + chance[1]
+        # clip top number to 100
+        if num > 100:
+            num = 100
+        chance_roll.append((chance[0], old_num, num))
+
+    return chance_roll
+
+def get_weapons_bonus_rarity():
+    chances = []
+    for data_id in weap_bonus_data:
+        if 'rarity' in weap_bonus_data[data_id]:
+            chances.append((weap_bonus_data[data_id]['name'], weap_bonus_data[data_id]['rarity']))
+
+    print chances
+
+    num = 0
+    chance_roll = []
+    for chance in chances:
+        old_num = num+1
+        num += 1+chance[1]
+        chance_roll.append((chance[0], old_num, num))
+
+    print chance_roll
+    return chance_roll
+
+def get_random_weapon_bonus():
+    bonus_rarity = get_weapons_bonus_rarity()
+
+    d100 = roll(1, 100)
+
+    breakpoints = [k[2] for k in bonus_rarity]
+    breakpoints.sort()
+
+    print breakpoints
+
+    i = bisect.bisect(breakpoints, d100)
+    res = bonus_rarity[i][0]
+    print "Random weapon bonus is " + res
+    return res
+
+def get_armor_material_rarity():
+    chances = []
+    for data_id in arm_mat_data:
+        if 'rarity' in arm_mat_data[data_id]:
+            chances.append((arm_mat_data[data_id]['name'], arm_mat_data[data_id]['rarity']))
+
+    print chances
+
+    num = 0
+    chance_roll = []
+    for chance in chances:
+        old_num = num+1
+        num += 1+chance[1]
+        chance_roll.append((chance[0], old_num, num))
+
+    #pad out to 100
+    print "Last number is " + str(num)
+    chance_roll.append(("None", num, 100))
+
+    print chance_roll
+
+    return chance_roll
+
+def get_random_armor_material():
+    material_rarity = get_armor_material_rarity()
+
+    d100 = roll(1, 100)
+
+    breakpoints = [k[2] for k in material_rarity]
+    breakpoints.sort()
+
+    print breakpoints
+
+    i = bisect.bisect(breakpoints, d100)
+    res = material_rarity[i][0]
+    print "Random material is " + res
+    return res
+
+
 def get_random_item():
     item_rarity = generate_item_rarity()
 
@@ -461,7 +552,29 @@ def get_random_item():
     i = bisect.bisect(breakpoints, d100)
     res = item_rarity[i][0]
     print "Random item rarity is " + res
-    return res
+
+    # generate item type
+    item_types = generate_item_type()
+    d100 = roll(1, 100)
+
+    breakpoints = [k[2] for k in item_types]
+    breakpoints.sort()
+
+    print breakpoints
+
+    i = bisect.bisect(breakpoints, d100)
+    it_type = item_types[i][0]
+    print "Random item type is " + it_type
+
+    # if armor, random material
+    if it_type == "Armor":
+        get_random_armor_material()
+
+    if it_type == "Weapons" and res is not "Mundane":
+        get_random_weapon_bonus()
+
+
+    # return res
 
 def get_monster_chances():
     chances = []
