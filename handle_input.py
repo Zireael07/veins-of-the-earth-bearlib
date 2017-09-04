d30f20abc46030c0378cec38ee95f0c6202f1f87
Zireael07 <zi@mail.com>
2017-09-04 09:00:03 +0200

    fix: fix stairs

--- handle_input.py
+++ handle_input.py
@@ -134,7 +134,7 @@ def game_handle_keys():
             return game_key_move('RIGHT')
 
         if key == blt.TK_PERIOD and blt.check(blt.TK_SHIFT):
-            if GAME.current_map[PLAYER.x][PLAYER.y] == 2: #.stairs:
+            if GAME.current_map[PLAYER.x][PLAYER.y] == 3: #.stairs:
                 GAME.next_level()
 
         # items
