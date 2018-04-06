26e573c2c823b953770bed85ecff8eaa316f5bc5
Zireael07 <zi@mail.com>
2018-04-06 17:11:15 +0200

    new: floodfill and scanline fill for noise mapgen

--- map/noisemap.py
+++ map/noisemap.py
@@ -1,6 +1,6 @@
 import libtcodpy as libtcod
 
-from map_common import print_map_string
+from map_common import print_map_string, Directions
 from tile_lookups import TileTypes, get_index
 
 class NoiseMapGenerator(object):
@@ -86,6 +86,82 @@ class NoiseMapGenerator(object):
 
         return self._map
 
+    def get_space_flood(self):
+        for x in range(0, self.map_width):
+            for y in range(0, self.map_height):
+                if self._map[x][y] == get_index(TileTypes.FLOOR): #2:
+                    # we start from top left
+                    print("Starting flood from x,y " + str(x) + "," + str(y))
+                    self.flood_fill(x, y)
+
+
+    def flood_fill(self, x, y):
+        """
+        flood fill the separate regions of the level
+        """
+        cave = set()
+        tile = (x, y)
+        to_fill = set([tile])
+        while to_fill:
+            tile = to_fill.pop()
+
+            if tile not in cave:
+                cave.add(tile)
+
+                self._map[tile[0]][tile[1]] = get_index(TileTypes.CRATE) #0
+
+                # check adjacent cells
+                x = tile[0]
+                y = tile[1]
+                north = (x+Directions.NORTH[0], y + Directions.NORTH[1])
+                south = (x+Directions.SOUTH[0], y + Directions.SOUTH[1])
+                east = (x + Directions.EAST[0], y + Directions.EAST[1])
+                west = (x + Directions.WEST[0], y + Directions.WEST[1])
+
+                for direction in [north, south, east, west]:
+
+                    if self._map[direction[0]][direction[1]] == get_index(TileTypes.FLOOR): #2:
+                        if direction not in to_fill and direction not in cave:
+                            to_fill.add(direction)
+
+    # 'scanline fill', being done line by line
+    def get_space_line(self):
+        for x in range(0, self.map_width):
+            for y in range(0, self.map_height):
+                if self._map[x][y] == get_index(TileTypes.FLOOR): #2:
+                    # we start from top left
+                    #print("Starting line fill from x,y " + str(x) + "," + str(y))
+                    self.line_fill(x, y)
+
+    # Simply expand east until we encounter a wall or end of map
+    def line_fill(self, x,y):
+        #initial_tile = (x,y)
+        cave = set()
+        tile = (x, y)
+        to_fill = set([tile])
+        while to_fill:
+            tile = to_fill.pop()
+
+            if tile not in cave:
+                cave.add(tile)
+
+                # to debug
+                self._map[tile[0]][tile[1]] = get_index(TileTypes.CRATE)  # 0
+
+                # check adjacent cells
+                x = tile[0]
+                y = tile[1]
+
+                east = (x + Directions.EAST[0], y + Directions.EAST[1])
+
+                if east[0] >= len(self._map[0]):
+                    #print("East is out of map, breaking loop")
+                    break
+
+                if self._map[east[0]][east[1]] == get_index(TileTypes.FLOOR): #2:
+                    if east not in to_fill and east not in cave:
+                        to_fill.add(east)
+
 
 if __name__ == '__main__':
 
@@ -97,16 +173,29 @@ if __name__ == '__main__':
 
     print_map_string(current_map)
 
+    # # test single line fill
+    # print("Linefill")
+    # map_gen.line_fill(0,0)
+    # print_map_string(current_map)
+
+    print("Test space line")
+    map_gen.get_space_line()
+
+    #print("After floodfill")
+    #map_gen.get_space_flood()
+
+    print_map_string(current_map)
+
     # changing hurst doesn't seem to do anything at least for 40x40
 
-    map_gen = NoiseMapGenerator(40, 40, 2, 1.5)
-    current_map = map_gen.generate_map()
-    print_map_string(current_map)
-
-    map_gen = NoiseMapGenerator(40, 40, 2, 1.0)
-    current_map = map_gen.generate_map()
-    print_map_string(current_map)
-
-    map_gen = NoiseMapGenerator(40, 40, 2, 3.0)
-    current_map = map_gen.generate_map()
-    print_map_string(current_map)
+    # map_gen = NoiseMapGenerator(40, 40, 2, 1.5)
+    # current_map = map_gen.generate_map()
+    # print_map_string(current_map)
+    #
+    # map_gen = NoiseMapGenerator(40, 40, 2, 1.0)
+    # current_map = map_gen.generate_map()
+    # print_map_string(current_map)
+    #
+    # map_gen = NoiseMapGenerator(40, 40, 2, 3.0)
+    # current_map = map_gen.generate_map()
+    # print_map_string(current_map)
